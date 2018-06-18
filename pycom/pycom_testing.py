c293588abb21bb937d3ce795f47a7a6dcd42bc30
Tomas Reyes <treyes4523@gmail.com>
2018-06-18 16:35:23 -0500

    I2C Work
    Attempting to get I2C to work. Not quite there. Likely an issue with addressing the SDA and SCL pins to their output. I have gotten GPIO to work referencing the "GXX" version of the pins. Need to check the logic output to see if the SDA and SCL pins are firing on a i2c.scan() call.

--- pycom/pycom_testing.py
+++ pycom/pycom_testing.py
@@ -1,13 +1,8 @@
-import machine,time
+from machine import I2C
 from machine import Pin
+import machine, time
 
+i2c = I2C(0, pins=('G15','G10'))     # create and use non-default PIN assignments (P10=SDA, P11=SCL)
+i2c.init(I2C.MASTER, baudrate=20000) # init as a master
 
-activate_pin = machine.Pin("G22",machine.Pin.OUT)
-
-try:
-    while True:
-        activate_pin.toggle()
-        time.sleep(1)
-        activate_pin.toggle()
-finally:
-    pass
+i2c.scan()
