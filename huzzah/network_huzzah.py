8a37affea9ad41a0b34a37c3833dfd0dc70902b8
Tomas Reyes <treyes4523@gmail.com>
2018-06-19 13:17:11 -0500

    Added timeout
    Added timeout exit condition, the device usually connects within 1 second, if not connected, allows use of the device with no network config.

--- huzzah/network_huzzah.py
+++ huzzah/network_huzzah.py
@@ -11,6 +11,8 @@ ap_if = network.WLAN(network.AP_IF)
 #turns off the access point, we don't need it right now
 ap_if.active(False)
 
+#timeout setup
+time_loop = 0
 #if it isn't already connected, start the process to connect
 if not sta_if.isconnected():
 		print('connecting to network...')
@@ -19,7 +21,8 @@ if not sta_if.isconnected():
 		#our network we are connecting to is called Pycom, with no password
 		sta_if.connect('Pycom', '')
 		#a while loop to wait for connection
-		while not sta_if.isconnected():
-			pass
+		while not sta_if.isconnected() and time_loop <= 10000:
+			time_loop += 1
+			time.sleep(0.001)
 #prints out the network configuration
 print('network config:', sta_if.ifconfig())
