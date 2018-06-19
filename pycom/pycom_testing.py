c40b356997ff8acaeae2afcd4aafa0722e0be0d4
Tomas Reyes <treyes4523@gmail.com>
2018-06-19 13:18:27 -0500

    Added motor teseting
    Motor testing, sometimes decides not to work, the analog discovery does not have a great power supply. Also transistor i supply sometimes causes voltage to drop to 0, aka, need smaller transistor. Otherwise, it works.

--- pycom/pycom_testing.py
+++ pycom/pycom_testing.py
@@ -1,8 +1,17 @@
-from machine import I2C
 from machine import Pin
+
 import machine, time
 
-i2c = I2C(0, pins=('G15','G10'))     # create and use non-default PIN assignments (P10=SDA, P11=SCL)
-i2c.init(I2C.MASTER, baudrate=20000) # init as a master
 
-i2c.scan()
+motor_pin = machine.Pin("G22",machine.Pin.OUT)
+
+try:
+    while True:
+        print("on")
+        motor_pin.toggle()
+        time.sleep(3)
+        print("off")
+        motor_pin.toggle()
+        time.sleep(3)
+finally:
+    pass
